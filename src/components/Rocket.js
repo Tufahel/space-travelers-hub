470214e3295432b7c9990c7dfa07ec678006f6e6
Tufahel <tufahel96@gmail.com>
2022-04-06 20:16:05 +0600

    reservation add and cancel done

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { reserveRocket } from '../redux/Rockets/RocketAction';
 import './RocketsComponentsStyle.css';
 
 const Rocket = (props) => {
   const {
-    name, image, description,
+    id, name, image, description, reserved,
   } = props;
+  const rockets = useSelector((state) => state.rocketsReducers);
+  const dispatch = useDispatch();
   return (
     <div className="grid-container">
       <div className="grid-item items-1">
@@ -16,7 +20,24 @@ const Rocket = (props) => {
       <div className="grid-item items-2">
         <h3 className="rocket_name">{name}</h3>
         <p>{description}</p>
-        <button type="button">Reservation</button>
+        {!reserved && (
+        <button
+          id={id}
+          type="button"
+          onClick={() => dispatch(reserveRocket(rockets, id))}
+        >
+          Reserve Rocket
+        </button>
+        )}
+        {reserved && (
+        <button
+          id={id}
+          type="button"
+          onClick={() => dispatch(reserveRocket(rockets, id))}
+        >
+          Cancel Reserve
+        </button>
+        )}
       </div>
     </div>
   );
@@ -25,5 +46,7 @@ Rocket.propTypes = {
   description: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 export default Rocket;
--- src/redux/Rockets/RocketAction.js
+++ src/redux/Rockets/RocketAction.js
@@ -1,6 +1,7 @@
 import fetchData from '../../api/RocketsApi';
 
 export const ADD_ROCKETS = 'space-travelers-hub/rockets/ADD_ROCKETS';
+export const ROCKETS_RESERVE = 'space-travelers-hub/rockets/RESERVE_ROCKETS';
 
 export const handleRocketsData = (data) => {
   const rockets = [];
@@ -16,6 +17,7 @@ export const handleRocketsData = (data) => {
       name,
       description,
       image,
+      reserved: false,
     };
     rockets.push(obj);
   });
@@ -27,5 +29,23 @@ export const renderLists = () => async (dispatch) => {
   dispatch({
     type: ADD_ROCKETS,
     payload: handleRocketsData(rockets),
+  });
+};
+
+export const reserveRocket = (state, id) => (dispatch) => {
+  const rockets = [...state];
+
+  for (let i = 0; i < rockets.length; i += 1) {
+    if (rockets[i].id === id) {
+      if (!rockets[i].reserved) {
+        rockets[i].reserved = true;
+      } else {
+        rockets[i].reserved = false;
+      }
+    }
+  }
+  dispatch({
+    type: ROCKETS_RESERVE,
+    payload: rockets,
   });
 };
