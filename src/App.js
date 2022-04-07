30e8541dbae80ea61573a5c0e2dbdf781ad25f88
Tufahel <tufahel96@gmail.com>
2022-04-07 18:15:00 +0600

    my rockets part added again

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Rockets from './components/Rockets';
 import Missions from './components/MissionsPage';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Rockets />} />
           <Route path="/missions" element={<Missions />} />
+          <Route path="/myprofile" element={<MyProfile />} />
         </Routes>
       </BrowserRouter>
     </div>
